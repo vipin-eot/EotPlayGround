06e21b10774539334b88480cfed7d95b12e4bd54
LAPTOP-AKRDN3UN\reeti <er.vipinyadav@gmail.com>
2024-07-11 18:30:00 +0530

    Initial commit

---
+++ GenAISamples/Phi-3Hugging Face/Program.cs
@@ -0,0 +1,57 @@
+﻿
+// 1. Download Phi-3 from Hugging Face
+// https://huggingface.co/microsoft/Phi-3-mini-4k-instruct
+
+
+using System;
+using System.Reflection.Emit;
+using System.Reflection;
+using Microsoft.ML.OnnxRuntimeGenAI;
+
+
+// folder location of the ONNX model file
+var modelPath = @"C:\Users\reeti\Downloads\dddd\Phi-3-mini-4k-instruct-onnx\directml\directml-int4-awq-block-128\model.onnx"; //Phi-3-mini-4k-instruct-onnx
+
+var model = new Model(modelPath);
+var tokenizer = new Tokenizer(model);
+
+var systemPrompt = "You are an AI assistant that helps people find information. Answer questions using a direct style. Do not share more information that the requested by the users.";
+
+// chat start
+Console.WriteLine(@"Ask your question. Type an empty string to Exit.");
+
+
+// chat loop
+while (true)
+{
+    // Get user question
+    Console.WriteLine();
+    Console.Write(@"Q: ");
+    var userQ = Console.ReadLine();
+    if (string.IsNullOrEmpty(userQ))
+    {
+        break;
+    }
+
+    // show phi3 response
+    Console.Write("Phi3: ");
+    var fullPrompt = $"<|system|>{systemPrompt}<|end|><|user|>{userQ}<|end|><|assistant|>";
+    var tokens = tokenizer.Encode(fullPrompt);
+
+    var generatorParams = new GeneratorParams(model);
+    generatorParams.SetSearchOption("max_length", 2048);
+    generatorParams.SetSearchOption("past_present_share_buffer", false);
+    generatorParams.SetInputSequences(tokens);
+
+    var generator = new Generator(model, generatorParams);
+    while (!generator.IsDone())
+    {
+        generator.ComputeLogits();
+        generator.GenerateNextToken();
+        var outputTokens = generator.GetSequence(0);
+        var newToken = outputTokens.Slice(outputTokens.Length - 1, 1);
+        var output = tokenizer.Decode(newToken);
+        Console.Write(output);
+    }
+    Console.WriteLine();
+}
