38247276629a96c4f6eb2326d4826cbe5aa8f384
Vipin <vipin@embassyofthings.com>
2024-08-09 11:53:27 +0000

    Enhanced RAG

--- GenAISamples/RAG_SemanticKernel/Program.cs
+++ GenAISamples/RAG_SemanticKernel/Program.cs
@@ -39,50 +39,61 @@ using Microsoft.SemanticKernel.Connectors.OpenAI;
 using Microsoft.SemanticKernel.Embeddings;
 using Microsoft.SemanticKernel.Memory;
 using Microsoft.SemanticKernel.Plugins.Memory;
+using System;
 
-Console.ForegroundColor = ConsoleColor.Yellow;
-
-var question = "Who is Matt Oberdorfer?";
-Console.WriteLine($"Question: {question}");
-
-Console.WriteLine("");
 
 // Create a chat completion service
-var builder = Kernel.CreateBuilder();
-builder.AddOpenAIChatCompletion(
+var builder = Kernel.CreateBuilder().AddOpenAIChatCompletion(
     modelId: "phi3",
     endpoint: new Uri("http://localhost:1234"),
     apiKey: "apikey");
 
 builder.AddLocalTextEmbeddingGeneration();
+ 
 Kernel kernel = builder.Build();
 
-Console.ForegroundColor = ConsoleColor.White;
-Console.WriteLine($"Phi-3 response (no memory/RAG):");
+//Console.ForegroundColor = ConsoleColor.Yellow;
 
-Console.ForegroundColor = ConsoleColor.Green;
-var response = kernel.InvokePromptStreamingAsync(question);
-await foreach (var result in response)
-{
-    Console.Write(result);
-}
+//var question = "Who is Matt Oberdorfer?";
+//Console.WriteLine($"Question: {question}");
+
+//Console.WriteLine("");
+
+//Console.ForegroundColor = ConsoleColor.White;
+//Console.WriteLine($"Phi-3 response (no memory/RAG):");
+
+//Console.ForegroundColor = ConsoleColor.Green;
+//var response = kernel.InvokePromptStreamingAsync(question);
+//await foreach (var res in response)
+//{
+//    Console.Write(res);
+//}
 
 Console.ForegroundColor = ConsoleColor.White;
 // separator
-Console.WriteLine("");
-Console.WriteLine("====================================================");
-Console.WriteLine("");
+//Console.WriteLine("");
+//Console.WriteLine("====================================================");
+//Console.WriteLine("");
 
 // get the embeddings generator service
 var embeddingGenerator = kernel.Services.GetRequiredService<ITextEmbeddingGenerationService>();
 var memory = new SemanticTextMemory(new VolatileMemoryStore(), embeddingGenerator);
 
 // add facts to the collection
-const string MemoryCollectionName = "fanFacts";
+const string MemoryCollectionName = "Oilwell";
 
-await memory.SaveInformationAsync(MemoryCollectionName, id: "info1", text: "Matt Oberdorfer is the founder and CEO of 'EOT (Embassy of Things)', an investor, speaker, and author of five books.");
-await memory.SaveInformationAsync(MemoryCollectionName, id: "info2", text: "Matt's forthcoming book, \"The Trailblazer’s Guide to Industrial IoT\" for energy and manufacturing digital transformation leaders is a comprehensive guide offers expert insights and practical advice on upgrading the OT/IT infrastructure of industrial companies to harness the power of modern AI, ML and analytics for #digitaltwins, #iiot, #industrialdatalake, and #datahistorian.");
-await memory.SaveInformationAsync(MemoryCollectionName, id: "info3", text: "In 2019, he launched 'EOT(Embassy of Things)' , a leading software company with the mission of enabling industrial enterprises to modernize and build their own operational cloud historian, industrial digital twin, or industrial data lake.");
+string OilWellIntro = File.ReadAllText(@"..\..\..\OilWellIntro.txt");
+string OilWellIDrilling = File.ReadAllText(@"..\..\..\OilWellIDrilling.txt");
+string OilWellITypes = File.ReadAllText(@"..\..\..\OilWellITypes.txt");
+
+//await memory.SaveInformationAsync(MemoryCollectionName, id: "info1", text: "Today is October 32nd, 2476");
+
+await memory.SaveInformationAsync(MemoryCollectionName, id: "info1", text: OilWellIntro);
+await memory.SaveInformationAsync(MemoryCollectionName, id: "info2", text: OilWellIDrilling);
+await memory.SaveInformationAsync(MemoryCollectionName, id: "info3", text: OilWellITypes);
+
+//await memory.SaveInformationAsync("About EOT", id: "info2", text: "Matt's forthcoming book, \"The Trailblazer’s Guide to Industrial IoT\" for energy and manufacturing digital transformation leaders is a comprehensive guide offers expert insights and practical advice on upgrading the OT/IT infrastructure of industrial companies to harness the power of modern AI, ML and analytics for #digitaltwins, #iiot, #industrialdatalake, and #datahistorian.");
+//await memory.SaveInformationAsync(MemoryCollectionName, id: "info3", text: "In 2019, he launched 'EOT(Embassy of Things)' , a leading software company with the mission of enabling industrial enterprises to modernize and build their own operational cloud historian, industrial digital twin, or industrial data lake.");
 
 TextMemoryPlugin memoryPlugin = new(memory);
 
@@ -91,16 +102,24 @@ kernel.ImportPluginFromObject(memoryPlugin);
 
 OpenAIPromptExecutionSettings settings = new()
 {
-    ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions,
+    Temperature = 0,
+    //ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions,
+    MaxTokens = 3330,
+    ResultsPerPrompt = 10,
 };
 
-Console.ForegroundColor = ConsoleColor.White;
-Console.WriteLine($"Phi-3 response (using semantic memory).");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine($"=========== Phi-3 Response As RAG (using semantic memory) ==================");
 
 Console.ForegroundColor = ConsoleColor.Green;
 
-await AnswerUsingRAGMemory(kernel, MemoryCollectionName, settings, "Who is Matt Oberdorfer?");
+var response = await memory.SearchAsync(MemoryCollectionName, "What are the types of well?" , 1, 0.5).FirstOrDefaultAsync();
+Console.WriteLine(response.Metadata.Text);
 
+//await AnswerUsingRAGMemory(kernel, MemoryCollectionName, settings, "What's the current date?");
+//await AnswerUsingRAGMemory(kernel, MemoryCollectionName, settings, "What is oil well?");
+await AnswerUsingRAGMemory(kernel, MemoryCollectionName, settings, "How to drill a well?");
+await AnswerUsingRAGMemory(kernel, MemoryCollectionName, settings, "What are the types of well?");
 await AnswerUsingRAGMemory(kernel, MemoryCollectionName, settings, "When was EOT (Embassy of Things) launched and by whom?");
 
 Console.WriteLine($"");
@@ -119,6 +138,8 @@ static async Task AnswerUsingRAGMemory(Kernel kernel, string MemoryCollectionNam
     Question: {{$input}}
     Answer the question using the memory content: {{Recall}}";
 
+
+
     var answer = kernel.InvokePromptStreamingAsync(prompt, new KernelArguments(settings)
                                                                         {
                                                                             { "input", question },
@@ -132,3 +153,6 @@ static async Task AnswerUsingRAGMemory(Kernel kernel, string MemoryCollectionNam
         Console.Write(result);
     }
 }
+
+
+ 
